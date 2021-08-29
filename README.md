2784b6a5451b7fac4523a4ed1835c29f7261a7a1
snaker95 <snaker95@126.com>
2021-08-29 21:52:15 +0800

    fix readme

--- README.md
+++ README.md
@@ -1,2 +1,15 @@
-# go
-golang 开发包
+# sync-go
+golang 开发包, 针对golang语言中goroutine进行安全封装
+## errgroup 包
+errgroup 为 google/x/sync/errgroup 的二次开发, 支持 recover(), 有效防止并发时, 
+由于并发协程产生 panic错误造成主协程退出, 尤其对于web 服务, 由并发造成主服务退出, 从而
+影响提供对外服务
+* 例如:
+    * InitErrgroupRecover(i interface{}) 主协程初始化时, 设置使用该包并发时产生panic,
+    recover()的处理方式,
+
+## gone 包
+gone为 关键字 go 进行二次封装, 支持 recover()统一处理; 避免影响主协程
+* 例如:
+    * InitGoneRecover(i interface{}) 主协程初始化时, 设置使用该包并发时产生panic,
+    recover()的处理方式, 
--- gone/go.go
+++ gone/go.go
@@ -14,25 +14,29 @@ import (
 
 func Go(f func()) {
 	go func() {
-		if goneRecover == nil {
-			goneRecover = defaultRecover
-		}
-		defer goneRecover()
+		defer func() {
+			if p := recover(); p != nil {
+				if goneRecover == nil {
+					goneRecover = defaultRecover
+				}
+				goneRecover(p)
+			}
+		}()
 		f()
 	}()
 }
 
 // goneRecover 封装 defer中recover()的处理方法
-var goneRecover func()
+var goneRecover func(p interface{})
 
 // InitGoneRecover goneRecover(), 可以在启动服务的做一次性初始化
-func InitGoneRecover(r func()) {
+func InitGoneRecover(r func(p interface{})) {
 	goneRecover = r
 }
 
 // defaultRecover call recover to hold back the case which has a panic
-func defaultRecover() {
-	if p := recover(); p != nil {
+func defaultRecover(p interface{}) {
+	if p != nil {
 		var st [4096]byte
 		n := runtime.Stack(st[:], false)
 		fmt.Printf("gone panic, err=%+v，stack trace:\n%v", p, string(st[:n]))
